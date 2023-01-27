95364cdb2eadfe0261c0f97319f3df85ac014539
shayan <shayanbali@gmail.com>
2023-01-27 09:42:34 +0330

    Add new user to website

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ app.secret_key = "shayan-bali"  # This should be kept secret
 
 
 # Add login function with JWT AUTH
-@app.route('/login', methods=['POST'])
+@app.route('/api/users/login', methods=['POST'])
 def login():
     # Get user's login credentials
     username = request.json['username']
@@ -36,6 +36,38 @@ def login():
     if user:
         # Generate JWT token
         token = jwt.encode({'username': user[1]}, app.secret_key, algorithm='HS256')
-        return jsonify({'token': token.decode('utf-8')})
+        return jsonify({'token': token.decode('utf-8')}), 200
     else:
         return jsonify({"msg": "Invalid credentials"}), 401
+
+
+# Check user JWT and verify it
+@app.route('/protected', methods=['GET'])
+def protected():
+    # Get JWT token from request headers
+    token = request.headers.get('Authorization')
+
+    # Verify JWT token
+    try:
+        payload = jwt.decode(token, app.secret_key, algorithms=['HS256'])
+        mycursor.execute("SELECT * FROM users WHERE username = %s", (payload['username'],))
+        user = mycursor.fetchone()
+        if user:
+            return jsonify({"msg": "Access granted"}), 200
+        else:
+            return jsonify({"msg": "Invalid token"}), 401
+    except jwt.DecodeError:
+        return jsonify({"msg": "Invalid token"}), 401
+
+# Add new user to website
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    # Get new user's credentials from request data
+    username = request.json['username']
+    password = request.json['password']
+
+    # Insert new user into the database
+    mycursor.execute("INSERT INTO users (username, password) VALUES (%s, %s)", (username, password))
+    mydb.commit()
+
+    return jsonify({"msg": "User created successfully"}), 200
