b3a3e8f0bc8a86a2045012b28ed9a28a585022fe
shayan <shayanbali@gmail.com>
2023-01-27 08:58:35 +0330

    Add login func with JWT

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import mysql
+import jwt
+from flask import Flask, request, jsonify
 
 # Connect to database
 HOST = "mysql-cc-hw1-shayanbali-shayanbali3-bfff.aivencloud.com"
@@ -7,14 +9,33 @@ USER = "avnadmin"
 PASSWORD = "AVNS_9XBFtm4jwXZQkSpUlig"
 DATABASE = "defaultdb"
 mydb = mysql.connector.connect(
-        host=HOST,
-        port=PORT,
-        user=USER,
-        password=PASSWORD,
-        database=DATABASE
-    )
+    host=HOST,
+    port=PORT,
+    user=USER,
+    password=PASSWORD,
+    database=DATABASE
+)
 
 mycursor = mydb.cursor(buffered=True)
 
+app = Flask(__name__)
+app.secret_key = "shayan-bali"  # This should be kept secret
 
 
+# Add login function with JWT AUTH
+@app.route('/login', methods=['POST'])
+def login():
+    # Get user's login credentials
+    username = request.json['username']
+    password = request.json['password']
+
+    # Authenticate user's credentials
+    mycursor.execute("SELECT * FROM users WHERE username = %s AND password = %s", (username, password))
+    user = mycursor.fetchone()
+
+    if user:
+        # Generate JWT token
+        token = jwt.encode({'username': user[1]}, app.secret_key, algorithm='HS256')
+        return jsonify({'token': token.decode('utf-8')})
+    else:
+        return jsonify({"msg": "Invalid credentials"}), 401
